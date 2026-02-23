cfdaec804527dbfb39517eb8ec010835c8015111
Your Name <youremail@example.com>
2026-02-23 18:31:43 +0530

    r1

---
+++ migrations/versions/e421fed2df5f_merge_all_heads.py
@@ -0,0 +1,24 @@
+"""merge_all_heads
+
+Revision ID: e421fed2df5f
+Revises: 552f64ea3d6d, g6h7i8j9k0l1
+Create Date: 2026-02-23 18:29:44.622786
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e421fed2df5f'
+down_revision = ('552f64ea3d6d', 'g6h7i8j9k0l1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
